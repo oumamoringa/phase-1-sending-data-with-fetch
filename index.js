a1528a4c53efcd3e4c3f0aed209de944d3178ebe
oumamoringa <richard.ouma@student.moringaschool>
2022-08-24 21:52:57 +0300

    tests passed

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-// Add your code here
+//Add your code here
 const submitData = (name, email)=>{
     return fetch("http://localhost:3000/users",
     {
         method: "POST",
         headers: {
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
+            "Accept": "application/json"
         },
         body: JSON.stringify({
             name,
